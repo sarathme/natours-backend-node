bffe37c2e7c9d884154b5c45b586f2270752bc20
Sarasraman <sarathsatheesh603@gmail.com>
2024-12-30 13:39:42 +0530

    Work on payments and bookings route and controller

---
+++ controllers/bookingController.js
@@ -0,0 +1,74 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Razorpay = require('razorpay');
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
+const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID, // Replace with your test Key ID
+  key_secret: process.env.RAZORPAY_API_KEY,
+});
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1) Get the currently booked tour
+  const tour = await Tour.findById(req.params.tourId);
+
+  console.log(tour);
+  // console.log(tour);
+
+  // 2) Create order
+  try {
+    const { price, id } = tour;
+    console.log(req.user);
+
+    const options = {
+      amount: price * 100, // Example: 50000 for ₹500
+      currency: 'INR', // INR, USD, etc.
+      receipt: `${Date.now()}-${req.user._id}`, // Unique identifier
+    };
+
+    const order = await razorpay.orders.create(options);
+    // 3) Create session as response
+    res.status(200).json({
+      status: 'success',
+      order,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+const createBookingCheckout = async (session) => {
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.display_items[0].amount / 100;
+  await Booking.create({ tour, user, price });
+};
+
+exports.webhookCheckout = (req, res, next) => {
+  const signature = req.headers['stripe-signature'];
+
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET,
+    );
+  } catch (err) {
+    return res.status(400).send(`Webhook error: ${err.message}`);
+  }
+
+  if (event.type === 'checkout.session.completed')
+    createBookingCheckout(event.data.object);
+
+  res.status(200).json({ received: true });
+};
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
