4fd072601f6e917320b176ddc59adde16b814741
Sarasraman <sarathsatheesh603@gmail.com>
2025-01-19 11:01:42 +0530

    Change user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.get('/logout', authController.logout);
+router.get('/logout', authController.protect, authController.logout);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
@@ -20,7 +20,7 @@ router.patch(
   '/updateMe',
   userController.uploadUserPhoto,
   userController.resizeUserPhoto,
-  userController.updateMe
+  userController.updateMe,
 );
 router.delete('/deleteMe', userController.deleteMe);
 
