ec65e25e90bf7e6d23f56c82349fe9156144c6ca
Sarasraman <sarathsatheesh603@gmail.com>
2025-01-19 12:01:25 +0530

    Deploy bug fix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const bookingController = require('./controllers/bookingController');
-const viewRouter = require('./routes/viewRoutes');
+// const viewRouter = require('./routes/viewRoutes');
 
 // Start express app
 const app = express();
@@ -111,7 +111,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/', viewRouter);
+// app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
