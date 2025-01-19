75b4364cddb298376a22933bdf6ee9d2cca0808b
Sarasraman <sarathsatheesh603@gmail.com>
2025-01-19 13:47:27 +0530

    Disable views for now erroe controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,19 +1,19 @@
 const AppError = require('./../utils/appError');
 
-const handleCastErrorDB = err => {
+const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
 
-const handleDuplicateFieldsDB = err => {
+const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
 
-const handleValidationErrorDB = err => {
-  const errors = Object.values(err.errors).map(el => el.message);
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
 
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
@@ -32,16 +32,16 @@ const sendErrorDev = (err, req, res) => {
       status: err.status,
       error: err,
       message: err.message,
-      stack: err.stack
+      stack: err.stack,
     });
   }
 
   // B) RENDERED WEBSITE
   console.error('ERROR 💥', err);
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: err.message
-  });
+  // return res.status(err.statusCode).render('error', {
+  //   title: 'Something went wrong!',
+  //   msg: err.message
+  // });
 };
 
 const sendErrorProd = (err, req, res) => {
@@ -51,7 +51,7 @@ const sendErrorProd = (err, req, res) => {
     if (err.isOperational) {
       return res.status(err.statusCode).json({
         status: err.status,
-        message: err.message
+        message: err.message,
       });
     }
     // B) Programming or other unknown error: don't leak error details
@@ -60,26 +60,26 @@ const sendErrorProd = (err, req, res) => {
     // 2) Send generic message
     return res.status(500).json({
       status: 'error',
-      message: 'Something went very wrong!'
+      message: 'Something went very wrong!',
     });
   }
 
   // B) RENDERED WEBSITE
   // A) Operational, trusted error: send message to client
-  if (err.isOperational) {
-    return res.status(err.statusCode).render('error', {
-      title: 'Something went wrong!',
-      msg: err.message
-    });
-  }
+  // if (err.isOperational) {
+  //   return res.status(err.statusCode).render('error', {
+  //     title: 'Something went wrong!',
+  //     msg: err.message
+  //   });
+  // }
   // B) Programming or other unknown error: don't leak error details
   // 1) Log error
   console.error('ERROR 💥', err);
   // 2) Send generic message
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: 'Please try again later.'
-  });
+  // return res.status(err.statusCode).render('error', {
+  //   title: 'Something went wrong!',
+  //   msg: 'Please try again later.'
+  // });
 };
 
 module.exports = (err, req, res, next) => {
